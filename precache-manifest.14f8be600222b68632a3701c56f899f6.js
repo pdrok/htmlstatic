f841992492dda8439c6d076968df5ed832e47e71
Pedro López <cde.luis@gmail.com>
2019-11-25 00:30:44 -0300

    FirstCommit
    Test

---
+++ precache-manifest.14f8be600222b68632a3701c56f899f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549f2053554bab213c04a694b21669e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40d00e1c665f2d0edb31",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ba18486da3a4850d04ba",
+    "url": "/static/js/2.ea8518a5.chunk.js"
+  },
+  {
+    "revision": "40d00e1c665f2d0edb31",
+    "url": "/static/js/main.4e695b7a.chunk.js"
+  },
+  {
+    "revision": "044f5c3185077a2e6ef6",
+    "url": "/static/js/runtime-main.2ed1afb4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
